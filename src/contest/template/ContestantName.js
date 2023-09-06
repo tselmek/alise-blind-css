08eb097f89bc6a2ea63c27677658f97be5d3a184
Antoine Jésus <antoine.jesus@telecomnancy.net>
2023-09-06 19:37:15 +0200

    Init canvas

---
+++ src/contest/template/ContestantName.js
@@ -0,0 +1,10 @@
+import styles from './ContestantName.module.css'
+
+export default function ContestantName() {
+  return (
+    <div className="container">
+      <div className={styles.square}></div>
+      <div className={styles.circle}></div>
+    </div>
+  )
+}
